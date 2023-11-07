51bc6a593ed3ca08646775008c43ea103fafc190
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-11-07 11:07:33 -0300

    REQ 6

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from news.models import News
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News, Category
+from news.forms import CategoryForms
 
 
 # Create your views here.
@@ -10,7 +11,17 @@ def home(request):
 
 def news_details(request, new_id):
     new = get_object_or_404(News, id=new_id)
-    categ = new.categories.all()
-    return render(
-        request, "news_details.html", {"new": new, "categories": categ}
-    )
+    ct = new.categories.all()
+    return render(request, "news_details.html", {"new": new, "categories": ct})
+
+
+def categories_form(request):
+    form = CategoryForms()
+    context_form = {"form": form}
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        Category.objects.create(name=name)
+        return redirect("home-page")
+
+    return render(request, "categories_form.html", context_form)
