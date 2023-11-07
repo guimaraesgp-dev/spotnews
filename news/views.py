80496311458e511745d23c7e4438159255c71cce
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-11-07 10:05:17 -0300

    REQ 5

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 
@@ -6,3 +6,11 @@ from news.models import News
 def home(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def news_details(request, new_id):
+    new = get_object_or_404(News, id=new_id)
+    categ = new.categories.all()
+    return render(
+        request, "news_details.html", {"new": new, "categories": categ}
+    )
