22db213146ab1df8e4d46f591407fca1a22101f4
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-11-07 09:15:28 -0300

    REQ 3

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -16,3 +17,21 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_title(title):
+    words = title.split()
+    if len(words) < 2:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, validators=[validate_title])
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to="img/", blank=True)
+    categories = models.ManyToManyField("Category")
+
+    def __str__(self):
+        return self.title
